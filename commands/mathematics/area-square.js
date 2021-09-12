bf5aa9997b1a6f86d8a1dfff32cc6888c51a253e
arcturus <aadhav0818@gmail.com>
2021-09-12 00:35:26 -0500

    efficiency changes

--- commands/mathematics/area-square.js
+++ commands/mathematics/area-square.js
@@ -1,20 +1,33 @@
 const Discord = require('discord.js');
 const config = require('../../config.json');
-const throwArg = require('../../objects/throwArg.js');
+const { argument, figure } = require('../../objects/throwArg.js');
 
 module.exports = {
-    name: 'area-square',
+    name: 'area',
     description: 'Calculates the area of a square',
     usage: `${config.prefix}area square [dim]`,
     example: `${config.prefix}area square 2`,
     async execute(client, message, args, Discord) {
-        throwArg.setFooter(`Try ${config.prefix}help ${this.name}`)
-        if(!args[0]) {return message.channel.send({ embeds: [throwArg] })}
+        [argument, figure].forEach(execption => {
+            execption.setFooter(`Try ${config.prefix}help ${this.name}`)
+        })
+        const areaFunction = ["square", "rect"];
+        if(!args[0]) {return message.channel.send({ embeds: [argument] })}
+        if(!areaFunction.includes(args[0])) {return message.channel.send({ embeds: [figure] })}
         const result = new Discord.MessageEmbed();
         result.setColor(config.colors.success);
-        result.setTitle('Area: Square');
-        result.addField('Result', `${Math.pow(args[0], 2)} units²`, false);
-        result.addField('Dimensions', `Side-Length: ${args[0]}`);
-        return message.channel.send({ embeds: [result] });
-    }
+        
+        class Area {
+            square(x) { return Math.pow(x, 2) }
+            rect(x, y) { return x * y } 
+        }
+
+        const areacalc = new Area();
+        if(args[0].toLowerCase() === "square") {
+            result.setTitle('Area: Square');
+            result.addField('Result', `${areacalc.square(args[1])} units²`, false);
+            result.addField('Dimensions', `Side-Length: ${args[0]}`);
+            return message.channel.send({ embeds: [result] });
+        }
+}
 }
--- objects/throwArg.js
+++ objects/throwArg.js
@@ -1,7 +1,13 @@
 const Discord = require("discord.js");
 const config = require('../config.json')
-const embed = new Discord.MessageEmbed();
-embed.setColor(config.colors.error);
-embed.setDescription('Missing Arguments');
+const arg = new Discord.MessageEmbed();
+arg.setColor(config.colors.error);
+arg.setDescription('**Error Code 001:** Missing Arguments');
+const fig = new Discord.MessageEmbed();
+fig.setColor(config.colors.error);
+fig.setDescription('**Error Code 002:** Invalid Figure');
 
-module.exports = embed;
+module.exports = {
+    argument: arg,
+    figure: fig
+}
